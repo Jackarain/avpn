220c7ea6f03b3dc354d2b4fc99870641e7a5107e
jack <jack.wgm@gmail.com>
2022-02-14 19:59:55 +0800

    更新到 Crypto++ 8.3

--- third_party/crypto/TestScripts/cryptest-android.sh
+++ third_party/crypto/TestScripts/cryptest-android.sh
@@ -3,42 +3,74 @@
 # ====================================================================
 # Tests Android cross-compiles
 #
-# Crypto++ Library is copyrighted as a compilation and (as of version 5.6.2)
-# licensed under the Boost Software License 1.0, while the individual files
-# in the compilation are all public domain.
-#
 # See http://www.cryptopp.com/wiki/Android_(Command_Line) for more details
 # ====================================================================
 
-PLATFORMS=(armeabi armeabi-v7a armeabi-v7a-hard armv7a-neon aarch64 mipsel x86 x86_64)
-RUNTIMES=(stlport-static stlport-shared gabi++-static gabi++-shared)
-for platform in ${PLATFORMS[@]}
+if [ -z "$(command -v ./setenv-android.sh)" ]; then
+    echo "Failed to locate setenv-android.sh"
+    [[ "$0" = "${BASH_SOURCE[0]}" ]] && exit 1 || return 1
+fi
+
+# Temp directory
+if [[ -z "$TMPDIR" ]]; then
+    TMPDIR="$HOME/tmp"
+    mkdir -p "$TMPDIR"
+fi
+
+MAKE_JOBS=2
+
+# Cleanup old artifacts
+rm -rf "$TMPDIR/build.failed" 2>/dev/null
+rm -rf "$TMPDIR/build.log" 2>/dev/null
+
+PLATFORMS=(armv7a aarch64 x86 x86_64)
+
+for platform in "${PLATFORMS[@]}"
 do
-	for runtime in ${RUNTIMES[@]}
-	do
-		make -f GNUmakefile-cross distclean > /dev/null 2>&1
+    # setenv-android.sh reads these two variables for configuration info.
+    export ANDROID_API="23"
+    export ANDROID_CPU="$platform"
 
-		MESSAGE="Testing for Android support of $platform using $runtime"
-		LEN=${#MESSAGE}
-		HEADER=$(seq  -f "*" -s '' $LEN)
+    make -f GNUmakefile-cross distclean > /dev/null 2>&1
 
-		echo
-		echo "$HEADER"
-		echo "$MESSAGE"
+    echo
+    echo "===================================================================="
+    echo "Testing for Android support of $platform"
 
-		# Test if we can set the environment for the platform
-		./setenv-android.sh "$platform" "$runtime" > /dev/null 2>&1
+    # Test if we can set the environment for the platform
+    if ! ./setenv-android.sh > /dev/null 2>&1;
+    then
+        echo
+        echo "There were problems testing $platform"
+        echo "$platform ==> SKIPPED" >> "$TMPDIR/build.log"
 
-		if [ "$?" -eq "0" ]; then
-			echo
-			echo "Building for $platform using $runtime..."
-			echo
+        continue
+    fi
 
-			. ./setenv-android.sh "$platform" "$runtime"
-			make -f GNUmakefile-cross static dynamic cryptest.exe
-		else
-			echo
-			echo "$platform not supported by Android"
-		fi
-	done
+    echo
+    echo "Building for $platform..."
+    echo
+
+    # run in subshell to not keep any envars
+    (
+        source ./setenv-android.sh
+        if make -k -j "$MAKE_JOBS" -f GNUmakefile-cross static dynamic cryptest.exe;
+        then
+            echo "$platform ==> SUCCESS" >> "$TMPDIR/build.log"
+        else
+            echo "$platform ==> FAILURE" >> "$TMPDIR/build.log"
+            touch "$TMPDIR/build.failed"
+        fi
+    )
 done
+
+echo ""
+echo "====================================================="
+cat "$TMPDIR/build.log"
+
+# let the script fail if any of the builds failed
+if [ -f "$TMPDIR/build.failed" ]; then
+    [[ "$0" = "${BASH_SOURCE[0]}" ]] && exit 1 || return 1
+fi
+
+[[ "$0" = "${BASH_SOURCE[0]}" ]] && exit 0 || return 0
--- third_party/crypto/TestScripts/cryptest-ios.sh
+++ third_party/crypto/TestScripts/cryptest-ios.sh
@@ -3,38 +3,91 @@
 # ====================================================================
 # Tests iOS cross-compiles
 #
-# Crypto++ Library is copyrighted as a compilation and (as of version 5.6.2)
-# licensed under the Boost Software License 1.0, while the individual files
-# in the compilation are all public domain.
-#
 # See http://www.cryptopp.com/wiki/iOS_(Command_Line) for more details
 # ====================================================================
 
-PLATFORMS=(iPhoneOS iPhoneSimulator WatchOS WatchSimulator AppleTVOS AppleTVSimulator)
-for platform in ${PLATFORMS[@]}
+if [ -z "$(command -v ./setenv-ios.sh)" ]; then
+    echo "Failed to locate setenv-ios.sh"
+    [[ "$0" = "${BASH_SOURCE[0]}" ]] && exit 1 || return 1
+fi
+
+# Temp directory
+if [[ -z "$TMPDIR" ]]; then
+    TMPDIR="$HOME/tmp"
+    mkdir "$TMPDIR"
+fi
+
+MAKE_JOBS=2
+
+# Cleanup old artifacts
+rm -rf "$TMPDIR/build.failed" 2>/dev/null
+rm -rf "$TMPDIR/build.log" 2>/dev/null
+
+# Hack a Bash data structure...
+PLATFORMS=()
+PLATFORMS+=("iPhoneOS:armv7")
+PLATFORMS+=("iPhoneOS:arm64")
+PLATFORMS+=("AppleTVOS:armv7")
+PLATFORMS+=("AppleTVOS:arm64")
+PLATFORMS+=("WatchOS:armv7")
+PLATFORMS+=("WatchOS:arm64")
+PLATFORMS+=("iPhoneSimulator:i386")
+PLATFORMS+=("iPhoneSimulator:x86_64")
+PLATFORMS+=("AppleTVSimulator:i386")
+PLATFORMS+=("AppleTVSimulator:x86_64")
+PLATFORMS+=("WatchSimulator:i386")
+PLATFORMS+=("WatchSimulator:x86_64")
+
+for platform in "${PLATFORMS[@]}"
 do
-	make -f GNUmakefile-cross distclean > /dev/null 2>&1
 
-	MESSAGE="Testing for Xcode support of $platform"
-	LEN=${#MESSAGE}
-	HEADER=$(seq  -f "*" -s '' $LEN)
+    sdk=$(echo "${platform[@]}" | awk -F':' '{print $1}')
+    cpu=$(echo "${platform[@]}" | awk -F':' '{print $2}')
 
-	echo
-	echo "$HEADER"
-	echo "$MESSAGE"
+    # setenv-ios.sh reads these two variables for configuration info.
+    export IOS_SDK="$sdk"
+    export IOS_CPU="$cpu"
 
-	# Test if we can set the environment for the platform
-	./setenv-ios.sh "$platform" > /dev/null 2>&1
+    make -f GNUmakefile-cross distclean > /dev/null 2>&1
 
-	if [ "$?" -eq "0" ]; then
-		echo
-		echo "Building for $platform..."
-		echo
+    echo
+    echo "====================================================="
+    echo "Testing for iOS support of $platform"
 
-		. ./setenv-ios.sh "$platform"
-		make -f GNUmakefile-cross static dynamic cryptest.exe
-	else
-		echo
-		echo "$platform not supported by Xcode"
-	fi
+    # Test if we can set the environment for the platform
+    if ! ./setenv-ios.sh > /dev/null 2>&1;
+    then
+        echo
+        echo "$platform not supported by Xcode"
+        echo "$platform ==> SKIPPED" >> "$TMPDIR/build.log"
+
+        continue
+    fi
+
+    echo
+    echo "Building for $platform..."
+    echo
+
+    # run in subshell to not keep any envars
+    (
+        source ./setenv-ios.sh
+        if make -k -j "$MAKE_JOBS" -f GNUmakefile-cross static dynamic cryptest.exe;
+        then
+            echo "$platform ==> SUCCESS" >> "$TMPDIR/build.log"
+        else
+            echo "$platform ==> FAILURE" >> "$TMPDIR/build.log"
+            touch "$TMPDIR/build.failed"
+        fi
+    )
 done
+
+echo ""
+echo "====================================================="
+cat "$TMPDIR/build.log"
+
+# let the script fail if any of the builds failed
+if [ -f "$TMPDIR/build.failed" ]; then
+    [[ "$0" = "${BASH_SOURCE[0]}" ]] && exit 1 || return 1
+fi
+
+[[ "$0" = "${BASH_SOURCE[0]}" ]] && exit 0 || return 0
