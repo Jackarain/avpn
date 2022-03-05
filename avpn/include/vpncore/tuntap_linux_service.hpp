243429df1eea5b4c895690db79e9231f7abea293
jack <jack.wgm@gmail.com>
2022-03-05 15:50:30 +0800

    Update

--- avpn/include/vpncore/tuntap_linux_service.hpp
+++ avpn/include/vpncore/tuntap_linux_service.hpp
@@ -99,15 +99,11 @@ inline int rtnl_wilddump_request_old(struct rtnl_handle *rth, int family, int ty
 
 namespace avpn
 {
-	inline std::optional<boost::asio::ip::network_v4> get_default_gateway()
+	inline std::tuple<boost::asio::ip::network_v4, std::string> default_gateway()
 	{
 		long Destination, Gateway, Flags, RefCnt, Use, Metric, Mask, MTU, Window, IRTT;
-		char iface[512] = { 0 };
-		char buf[1024] = { 0 };
 		FILE* file;
-
-		memset(iface, 0, sizeof(iface));
-		memset(buf, 0, sizeof(buf));
+		std::string iface;
 
 		file = fopen("/proc/net/route", "r");
 		if (!file)
@@ -117,12 +113,13 @@ namespace avpn
 		long lowest_metric = std::numeric_limits<long>::max();
 		boost::asio::ip::network_v4 net;
 
+		char buf[1024] = { 0 };
+		memset(buf, 0, sizeof(buf));
 		while (fgets(buf, sizeof(buf), file))
 		{
-			LOG_DBG << buf;
-
+			char tmp[512] = { 0 };
 			if (sscanf(buf, "%16s %lx %lx %lx %ld %ld %ld %lx %ld %ld %ld",
-				iface, &Destination, &Gateway, &Flags, &RefCnt,
+				tmp, &Destination, &Gateway, &Flags, &RefCnt,
 				&Use, &Metric, &Mask, &MTU, &Window, &IRTT) == 11)
 			{
 				if (Destination == 0 && Mask == 0 && Metric < lowest_metric)
@@ -133,6 +130,7 @@ namespace avpn
 					boost::asio::ip::address_v4 mask{ ntohl(Mask) };
 
 					net = boost::asio::ip::network_v4(gw, mask);
+					iface = tmp;
 				}
 			}
 		}
@@ -143,7 +141,19 @@ namespace avpn
 		LOG_DBG << "Default gateway: " << net.address().to_string()
 			<< ", lowest metric: " << lowest_metric;
 
-		return net;
+		return { net, iface };
+	}
+
+	inline std::optional<boost::asio::ip::network_v4> get_default_gateway()
+	{
+		[[maybe_unused]] auto [net, iface] = default_gateway();
+		return { net };
+	}
+
+	inline std::optional<std::string> get_default_gateway_iface()
+	{
+		[[maybe_unused]] auto [net, iface] = default_gateway();
+		return { iface };
 	}
 
 	template <typename ReturnType>
