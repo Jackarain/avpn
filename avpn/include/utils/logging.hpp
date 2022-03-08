15906bf365833f855ebf9d5622339c0dfee87581
jack <jack.wgm@gmail.com>
2022-03-08 21:06:33 +0800

    Remove macro

--- avpn/include/utils/logging.hpp
+++ avpn/include/utils/logging.hpp
@@ -53,13 +53,11 @@
 #	endif
 #endif
 
-#if defined(__cpp_lib_format)
+#if !defined(__cpp_lib_format)
 #	include <format>
-#	define format std::format
-#	define format_to std::format_to
 #endif
 
-#if !defined(__cpp_lib_format)
+#if !!defined(__cpp_lib_format)
 #ifdef _MSC_VER
 #	pragma warning(push)
 #	pragma warning(disable: 4244 4127)
@@ -74,8 +72,7 @@
 #include <fmt/printf.h>
 #include <fmt/format.h>
 
-#define format fmt::format
-#define format_to fmt::format_to
+namespace std {	using namespace fmt; }
 
 #ifdef __clang__
 #pragma clang diagnostic pop
@@ -494,12 +491,12 @@ public:
 			std::filesystem::path filename;
 
 			if constexpr (LOG_MAXFILE_SIZE <= 0) {
-				auto logfile = format("{:04d}{:02d}{:02d}-{:02d}.log",
+				auto logfile = std::format("{:04d}{:02d}{:02d}-{:02d}.log",
 					ptm->tm_year + 1900, ptm->tm_mon + 1, ptm->tm_mday, ptm->tm_hour);
 				filename = logpath / logfile;
 			} else {
 				auto utc_time = std::mktime(ptm);
-				auto logfile = format("{:04d}{:02d}{:02d}-{}.log",
+				auto logfile = std::format("{:04d}{:02d}{:02d}-{}.log",
 					ptm->tm_year + 1900, ptm->tm_mon + 1, ptm->tm_mday, utc_time);
 				filename = logpath / logfile;
 			}
@@ -623,13 +620,13 @@ inline void logger_output_console__([[maybe_unused]] bool disable_cout,
 #elif !defined(DISABLE_LOGGER_TO_CONSOLE)
 	std::string out;
 	if (level == _logger_info_id__)
-		format_to(std::back_inserter(out), "\033[32m{}\033[0m{}", prefix, message);
+		std::format_to(std::back_inserter(out), "\033[32m{}\033[0m{}", prefix, message);
 	else if (level == _logger_debug_id__)
-		format_to(std::back_inserter(out), "\033[1;32m{}\033[0m{}", prefix, message);
+		std::format_to(std::back_inserter(out), "\033[1;32m{}\033[0m{}", prefix, message);
 	else if (level == _logger_warn_id__)
-		format_to(std::back_inserter(out), "\033[1;33m{}\033[0m{}", prefix, message);
+		std::format_to(std::back_inserter(out), "\033[1;33m{}\033[0m{}", prefix, message);
 	else if (level == _logger_error_id__)
-		format_to(std::back_inserter(out), "\033[1;31m{}\033[0m{}", prefix, message);
+		std::format_to(std::back_inserter(out), "\033[1;31m{}\033[0m{}", prefix, message);
 	std::cout << out;
 	std::cout.flush();
 #endif
@@ -829,7 +826,7 @@ public:
 	{
 		if (!logging_flag())
 			return *this;
-		format_to(std::back_inserter(out_), "{}", v);
+		std::format_to(std::back_inserter(out_), "{}", v);
 		return *this;
 	}
 
@@ -893,7 +890,7 @@ public:
 	{
 		if (!logging_flag())
 			return *this;
-		format_to(std::back_inserter(out_), "{:#010x}", (std::size_t)v);
+		std::format_to(std::back_inserter(out_), "{:#010x}", (std::size_t)v);
 		return *this;
 	}
 	inline logger___& operator<<(const boost::posix_time::ptime& p) noexcept
@@ -906,21 +903,21 @@ public:
 			auto date = p.date().year_month_day();
 			auto time = p.time_of_day();
 
-			format_to(std::back_inserter(out_), "{:04}", static_cast<unsigned int>(date.year));
-			format_to(std::back_inserter(out_), "-{:02}", date.month.as_number());
-			format_to(std::back_inserter(out_), "-{:02}", date.day.as_number());
+			std::format_to(std::back_inserter(out_), "{:04}", static_cast<unsigned int>(date.year));
+			std::format_to(std::back_inserter(out_), "-{:02}", date.month.as_number());
+			std::format_to(std::back_inserter(out_), "-{:02}", date.day.as_number());
 
-			format_to(std::back_inserter(out_), " {:02}", time.hours());
-			format_to(std::back_inserter(out_), ":{:02}", time.minutes());
-			format_to(std::back_inserter(out_), ":{:02}", time.seconds());
+			std::format_to(std::back_inserter(out_), " {:02}", time.hours());
+			std::format_to(std::back_inserter(out_), ":{:02}", time.minutes());
+			std::format_to(std::back_inserter(out_), ":{:02}", time.seconds());
 
 			auto ms = time.total_milliseconds() % 1000;		// milliseconds.
 			if (ms != 0)
-				format_to(std::back_inserter(out_), ".{:03}", ms);
+				std::format_to(std::back_inserter(out_), ".{:03}", ms);
 		}
 		else
 		{
-			format_to(std::back_inserter(out_), "NOT A DATE TIME");
+			std::format_to(std::back_inserter(out_), "NOT A DATE TIME");
 		}
 
 		return *this;
@@ -985,6 +982,3 @@ public:
 #define INIT_ASYNC_LOGGING() void
 
 #endif
-
-#undef format
-#undef format_to
