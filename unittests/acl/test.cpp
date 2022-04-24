aa91acc835154bf4919702828d52555d09e5d047
jack <jack.wgm@gmail.com>
2022-04-24 10:35:38 +0800

    Add acl unittests

---
+++ unittests/acl/test.cpp
@@ -0,0 +1,39 @@
+﻿#define BOOST_TEST_MAIN
+
+#ifdef USE_MIMALLOC
+
+#ifdef MI_OVERRIDE
+#	include <mimalloc.h>
+#else
+#	include <mimalloc-new-delete.h>
+#endif
+
+#ifdef _WIN32
+#	include <mimalloc-new-delete.h>
+#endif
+
+#endif // USE_MIMALLOC
+
+#include <boost/test/included/unit_test.hpp>
+#include <cstdlib>
+#include <ctime>
+
+#include "utils/acl.hpp"
+
+BOOST_AUTO_TEST_CASE(acl_test)
+{
+		auto addr = boost::asio::ip::address_v4::from_string("36.128.0.0");
+		boost::asio::ip::network_v4 net(addr, 10);
+		acl_util::lpm_table table;
+		acl_util::lpm_tag tag((void*)0xa);
+
+		table.insert(net, tag);
+
+		addr = boost::asio::ip::address_v4::from_string("36.129.0.0");
+		auto target = table.lookup(addr);
+		BOOST_TEST(target == (void*)0xa);
+
+		addr = boost::asio::ip::address_v4::from_string("10.0.0.2");
+		target = table.lookup(addr);
+		BOOST_TEST(target == (void*)0);
+}
