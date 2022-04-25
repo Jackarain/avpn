82853f45a9abf3f7c99c0b3b84abdcb5c4c31229
jack <jack.wgm@gmail.com>
2022-04-25 10:16:53 +0800

    Add dh for keyexchange

--- avpn/include/utils/crypto.hpp
+++ avpn/include/utils/crypto.hpp
@@ -9,6 +9,10 @@
 
 #include "utils/xxhash.hpp"
 
+#include <cryptopp/dh.h>
+#include <cryptopp/dh2.h>
+#include <cryptopp/osrng.h>
+
 #include <cstddef>
 #include <string>
 #include <string_view>
@@ -18,6 +22,64 @@
 
 
 namespace crypto_util {
+
+class dh_keyexchange
+{
+public:
+    dh_keyexchange()
+		: p_("0xB10B8F96A080E01DDE92DE5EAE5D54EC52C99FBCFB06A3C6"
+			"9A6A9DCA52D23B616073E28675A23D189838EF1E2EE652C0"
+			"13ECB4AEA906112324975C3CD49B83BFACCBDD7D90C4BD70"
+			"98488E9C219A73724EFFD6FAE5644738FAA31A4FF55BCCC0"
+			"A151AF5F0DC8B4BD45BF37DF365C1A65E68CFDA76D4DA708"
+			"DF1FB2BC2E4A4371")
+		, g_("0xA4D1CBD5C3FD34126765A442EFB99905F8104DD258AC507F"
+			"D6406CFF14266D31266FEA1E5C41564B777E690F5504F213"
+			"160217B4B01B886A5E91547F9E2749F4D7FBD7D3B9A92EE1"
+			"909D0D2263F80A76A6A24C087A091F531DBF0A0169B6A28A"
+			"D662A4D18E73AFA32D779D5918D08BC8858F4DCEF97C2A24"
+			"855E6EEB22B3B2E5")
+		, q_("0xF518AA8781A8DF278ABA4E7D64B7CB9D49462353")
+	{
+		CryptoPP::AutoSeededRandomPool prng;
+
+		dh_.AccessGroupParameters().Initialize(p_, q_, g_);
+		dh_.AccessGroupParameters().ValidateGroup(prng, 3);
+
+		privateKey_.resize(dh_.PrivateKeyLength());
+		publicKey_.resize(dh_.PublicKeyLength());
+
+		dh_.GenerateKeyPair(prng, privateKey_, publicKey_);
+	}
+    ~dh_keyexchange() = default;
+
+public:
+	std::string_view PublicKey()
+	{
+		return { (char*)publicKey_.data(), publicKey_.size() };
+	}
+
+	std::string_view GenerateSharedKey(std::string_view otherPubKey)
+	{
+		CryptoPP::SecByteBlock pubKey(
+			reinterpret_cast<const CryptoPP::byte*>(otherPubKey.data()), otherPubKey.size());
+
+		shared_.resize(dh_.AgreedValueLength());
+		if (!dh_.Agree(shared_, privateKey_, pubKey))
+			return {};
+
+		return { (char*)shared_.data(), shared_.size() };
+	}
+
+private:
+	CryptoPP::Integer p_;
+	CryptoPP::Integer g_;
+	CryptoPP::Integer q_;
+	CryptoPP::DH dh_;
+	CryptoPP::SecByteBlock privateKey_;
+	CryptoPP::SecByteBlock publicKey_;
+	CryptoPP::SecByteBlock shared_;
+};
 
 class stream_crypto
 {
@@ -76,7 +138,7 @@ public:
 private:
     std::seed_seq m_seed;
     std::mt19937 m_mt19937;
-    std::uniform_int_distribution<uint8_t> m_distribution;
+    std::uniform_int_distribution<int> m_distribution;
 };
 
 }
