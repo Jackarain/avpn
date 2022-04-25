02f9d49e8aaf5d67780a3b0ed1dfe823f47418ba
jack <jack.wgm@gmail.com>
2022-04-25 19:00:16 +0800

    使用xxhash官方版本

--- avpn/include/utils/crypto.hpp
+++ avpn/include/utils/crypto.hpp
@@ -7,6 +7,10 @@
 
 #pragma once
 
+#ifndef XXH_INLINE_ALL
+#	define XXH_INLINE_ALL
+#endif // !XXH_INLINE_ALL
+
 #include "utils/xxhash.hpp"
 
 #include <cryptopp/dh.h>
@@ -23,167 +27,167 @@
 
 namespace crypto_util {
 
-class dh_keyexchange
-{
-public:
-	dh_keyexchange(std::string_view static_private_key = {}, std::string_view static_public_key = {})
-		: p_("0xB10B8F96A080E01DDE92DE5EAE5D54EC52C99FBCFB06A3C6"
-			"9A6A9DCA52D23B616073E28675A23D189838EF1E2EE652C0"
-			"13ECB4AEA906112324975C3CD49B83BFACCBDD7D90C4BD70"
-			"98488E9C219A73724EFFD6FAE5644738FAA31A4FF55BCCC0"
-			"A151AF5F0DC8B4BD45BF37DF365C1A65E68CFDA76D4DA708"
-			"DF1FB2BC2E4A4371")
-		, g_("0xA4D1CBD5C3FD34126765A442EFB99905F8104DD258AC507F"
-			"D6406CFF14266D31266FEA1E5C41564B777E690F5504F213"
-			"160217B4B01B886A5E91547F9E2749F4D7FBD7D3B9A92EE1"
-			"909D0D2263F80A76A6A24C087A091F531DBF0A0169B6A28A"
-			"D662A4D18E73AFA32D779D5918D08BC8858F4DCEF97C2A24"
-			"855E6EEB22B3B2E5")
-		, q_("0xF518AA8781A8DF278ABA4E7D64B7CB9D49462353")
-        , dh2_(dh_)
+	class dh_keyexchange
 	{
-		CryptoPP::AutoSeededRandomPool prng;
-
-		dh_.AccessGroupParameters().Initialize(p_, q_, g_);
-		dh_.GetGroupParameters().ValidateGroup(prng, 3);
-
-        CryptoPP::DH2& dh2 = dh2_;
-
-		if (static_private_key.empty() || static_public_key.empty())
+	public:
+		dh_keyexchange(std::string_view static_private_key = {}, std::string_view static_public_key = {})
+			: p_("0xB10B8F96A080E01DDE92DE5EAE5D54EC52C99FBCFB06A3C6"
+				"9A6A9DCA52D23B616073E28675A23D189838EF1E2EE652C0"
+				"13ECB4AEA906112324975C3CD49B83BFACCBDD7D90C4BD70"
+				"98488E9C219A73724EFFD6FAE5644738FAA31A4FF55BCCC0"
+				"A151AF5F0DC8B4BD45BF37DF365C1A65E68CFDA76D4DA708"
+				"DF1FB2BC2E4A4371")
+			, g_("0xA4D1CBD5C3FD34126765A442EFB99905F8104DD258AC507F"
+				"D6406CFF14266D31266FEA1E5C41564B777E690F5504F213"
+				"160217B4B01B886A5E91547F9E2749F4D7FBD7D3B9A92EE1"
+				"909D0D2263F80A76A6A24C087A091F531DBF0A0169B6A28A"
+				"D662A4D18E73AFA32D779D5918D08BC8858F4DCEF97C2A24"
+				"855E6EEB22B3B2E5")
+			, q_("0xF518AA8781A8DF278ABA4E7D64B7CB9D49462353")
+			, dh2_(dh_)
 		{
-			staticPrivateKey_.resize(dh2.StaticPrivateKeyLength());
-			staticPublicKey_.resize(dh2.StaticPublicKeyLength());
+			CryptoPP::AutoSeededRandomPool prng;
 
-			dh2.GenerateStaticKeyPair(prng, staticPrivateKey_, staticPublicKey_);
+			dh_.AccessGroupParameters().Initialize(p_, q_, g_);
+			dh_.GetGroupParameters().ValidateGroup(prng, 3);
+
+			CryptoPP::DH2& dh2 = dh2_;
+
+			if (static_private_key.empty() || static_public_key.empty())
+			{
+				staticPrivateKey_.resize(dh2.StaticPrivateKeyLength());
+				staticPublicKey_.resize(dh2.StaticPublicKeyLength());
+
+				dh2.GenerateStaticKeyPair(prng, staticPrivateKey_, staticPublicKey_);
+			}
+			else
+			{
+				staticPrivateKey_ = CryptoPP::SecByteBlock(reinterpret_cast<const CryptoPP::byte*>(
+					static_private_key.data()), static_private_key.size());
+				staticPublicKey_ = CryptoPP::SecByteBlock(reinterpret_cast<const CryptoPP::byte*>(
+					static_public_key.data()), static_public_key.size());
+			}
+
+			ephemeralPrivateKey_.resize(dh2.EphemeralPrivateKeyLength());
+			ephemeralPublicKey_.resize(dh2.EphemeralPublicKeyLength());
+
+			dh2.GenerateEphemeralKeyPair(prng, ephemeralPrivateKey_, ephemeralPublicKey_);
 		}
-		else
+		~dh_keyexchange() = default;
+
+	public:
+		std::string_view StaticPublicKey()
 		{
-			staticPrivateKey_ = CryptoPP::SecByteBlock(reinterpret_cast<const CryptoPP::byte*>(
-				static_private_key.data()), static_private_key.size());
-			staticPublicKey_ = CryptoPP::SecByteBlock(reinterpret_cast<const CryptoPP::byte*>(
-				static_public_key.data()), static_public_key.size());
+			return { (char*)staticPublicKey_.data(), staticPublicKey_.size() };
 		}
 
-		ephemeralPrivateKey_.resize(dh2.EphemeralPrivateKeyLength());
-		ephemeralPublicKey_.resize(dh2.EphemeralPublicKeyLength());
+		std::string_view EphemeralPublicKey()
+		{
+			return { (char*)ephemeralPublicKey_.data(), ephemeralPublicKey_.size() };
+		}
 
-		dh2.GenerateEphemeralKeyPair(prng, ephemeralPrivateKey_, ephemeralPublicKey_);
-	}
-    ~dh_keyexchange() = default;
+		std::string_view GenerateSharedKey(std::string_view otherPubKey)
+		{
+			CryptoPP::SecByteBlock pubKey(
+				reinterpret_cast<const CryptoPP::byte*>(otherPubKey.data()), otherPubKey.size());
 
-public:
-	std::string_view StaticPublicKey()
+			shared_.resize(dh_.AgreedValueLength());
+			if (!dh_.Agree(shared_, ephemeralPrivateKey_, pubKey))
+				return {};
+
+			return { (char*)shared_.data(), shared_.size() };
+		}
+
+		std::string_view GenerateSharedKey(std::string_view otherStaticPubKey, std::string_view otherEphemeralPubKey)
+		{
+			shared_.resize(dh2_.AgreedValueLength());
+
+			CryptoPP::SecByteBlock spubKey(
+				reinterpret_cast<const CryptoPP::byte*>(otherStaticPubKey.data()), otherStaticPubKey.size());
+			CryptoPP::SecByteBlock epubKey(
+				reinterpret_cast<const CryptoPP::byte*>(otherEphemeralPubKey.data()), otherEphemeralPubKey.size());
+
+			if (!dh2_.Agree(shared_, staticPrivateKey_, ephemeralPrivateKey_, spubKey, epubKey))
+				return {};
+
+			return { (char*)shared_.data(), shared_.size() };
+		}
+
+	private:
+		CryptoPP::Integer p_;
+		CryptoPP::Integer g_;
+		CryptoPP::Integer q_;
+
+		CryptoPP::SecByteBlock staticPrivateKey_;
+		CryptoPP::SecByteBlock staticPublicKey_;
+
+		CryptoPP::SecByteBlock ephemeralPublicKey_;
+		CryptoPP::SecByteBlock ephemeralPrivateKey_;
+
+		CryptoPP::SecByteBlock shared_;
+
+		CryptoPP::DH dh_;
+		CryptoPP::DH2 dh2_;
+	};
+
+	class stream_crypto
 	{
-		return { (char*)staticPublicKey_.data(), staticPublicKey_.size() };
-	}
+		stream_crypto(const stream_crypto&) = delete;
+		stream_crypto& operator=(const stream_crypto&) = delete;
 
-	std::string_view EphemeralPublicKey()
-	{
-		return { (char*)ephemeralPublicKey_.data(), ephemeralPublicKey_.size() };
-	}
+		class CSPRNG
+		{
+			CSPRNG(const CSPRNG&) = delete;
+			CSPRNG& operator=(const CSPRNG&) = delete;
 
-	std::string_view GenerateSharedKey(std::string_view otherPubKey)
-	{
-		CryptoPP::SecByteBlock pubKey(
-			reinterpret_cast<const CryptoPP::byte*>(otherPubKey.data()), otherPubKey.size());
+		public:
+			using result_type = uint64_t;
 
-		shared_.resize(dh_.AgreedValueLength());
-		if (!dh_.Agree(shared_, ephemeralPrivateKey_, pubKey))
-			return {};
+			CSPRNG(std::mt19937& mt)
+				: mt_(mt)
+			{}
+			~CSPRNG() = default;
 
-		return { (char*)shared_.data(), shared_.size() };
-	}
+			result_type operator()()
+			{
+				auto result = mt_();
+				const char* p = reinterpret_cast<const char*>(&result);
+				return (result_type)XXH32(p, sizeof(result), 0);
+			}
 
-	std::string_view GenerateSharedKey(std::string_view otherStaticPubKey, std::string_view otherEphemeralPubKey)
-	{
-		shared_.resize(dh2_.AgreedValueLength());
+			static constexpr result_type min()
+			{
+				return std::numeric_limits<result_type>::min();
+			}
 
-		CryptoPP::SecByteBlock spubKey(
-			reinterpret_cast<const CryptoPP::byte*>(otherStaticPubKey.data()), otherStaticPubKey.size());
-		CryptoPP::SecByteBlock epubKey(
-			reinterpret_cast<const CryptoPP::byte*>(otherEphemeralPubKey.data()), otherEphemeralPubKey.size());
+			static constexpr result_type max()
+			{
+				return std::numeric_limits<result_type>::max();
+			}
 
-		if (!dh2_.Agree(shared_, staticPrivateKey_, ephemeralPrivateKey_, spubKey, epubKey))
-			return {};
+		private:
+			std::mt19937& mt_;
+		};
 
-		return { (char*)shared_.data(), shared_.size() };
-	}
+	public:
+		explicit stream_crypto(std::string_view passwd)
+			: m_seed(passwd.begin(), passwd.end())
+			, m_mt19937(m_seed)
+			, m_distribution(0, 255)
+		{}
+		~stream_crypto() = default;
 
-private:
-	CryptoPP::Integer p_;
-	CryptoPP::Integer g_;
-	CryptoPP::Integer q_;
+		void perform(std::span<std::byte> content)
+		{
+			CSPRNG rng(m_mt19937);
+			for (auto& c : content)
+				c = c ^ static_cast<std::byte>(m_distribution(rng));
+		}
 
-	CryptoPP::SecByteBlock staticPrivateKey_;
-	CryptoPP::SecByteBlock staticPublicKey_;
-
-    CryptoPP::SecByteBlock ephemeralPublicKey_;
-    CryptoPP::SecByteBlock ephemeralPrivateKey_;
-
-	CryptoPP::SecByteBlock shared_;
-
-	CryptoPP::DH dh_;
-	CryptoPP::DH2 dh2_;
-};
-
-class stream_crypto
-{
-    stream_crypto(const stream_crypto&) = delete;
-	stream_crypto& operator=(const stream_crypto&) = delete;
-
-    class CSPRNG
-    {
-        CSPRNG(const CSPRNG&) = delete;
-        CSPRNG& operator=(const CSPRNG&) = delete;
-
-    public:
-        using result_type = uint64_t;
-
-        CSPRNG(std::mt19937& mt)
-            : mt_(mt)
-        {}
-        ~CSPRNG() = default;
-
-        result_type operator()()
-        {
-            auto result = mt_();
-            const char* p = reinterpret_cast<const char*>(&result);
-            return xxh::xxhash3<64>((const void*)p, sizeof(result));
-        }
-
-        static constexpr result_type min()
-        {
-            return std::numeric_limits<result_type>::min();
-        }
-
-        static constexpr result_type max()
-        {
-            return std::numeric_limits<result_type>::max();
-        }
-
-    private:
-        std::mt19937& mt_;
-    };
-
-public:
-    explicit stream_crypto(std::string_view passwd)
-        : m_seed(passwd.begin(), passwd.end())
-        , m_mt19937(m_seed)
-        , m_distribution(0, 255)
-    {}
-    ~stream_crypto() = default;
-
-    void perform(std::span<std::byte> content)
-    {
-        CSPRNG rng(m_mt19937);
-        for (auto& c : content)
-            c = c ^ static_cast<std::byte>(m_distribution(rng));
-    }
-
-private:
-    std::seed_seq m_seed;
-    std::mt19937 m_mt19937;
-    std::uniform_int_distribution<int> m_distribution;
-};
+	private:
+		std::seed_seq m_seed;
+		std::mt19937 m_mt19937;
+		std::uniform_int_distribution<int> m_distribution;
+	};
 
 }
