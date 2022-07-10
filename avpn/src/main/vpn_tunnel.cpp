9b54074763302127f84475b8a2d9adc5182847dd
jack <jack.wgm@gmail.com>
2022-07-10 20:11:46 +0800

    Improve transfer policy.

--- avpn/src/main/vpn_tunnel.cpp
+++ avpn/src/main/vpn_tunnel.cpp
@@ -400,12 +400,7 @@ namespace avpn {
 					m_num_recv_packet, m_num_send_packet, timestamp);
 				auto ptr = std::make_shared<vpn_packet>(std::move(pkt));
 
-				if ((std::rand() % 2 == 0 &&
-					m_ipproto == Proto::avpn_mix) ||
-					m_ipproto == Proto::avpn_udp)
-					udp_write_pkt(ptr);
-				else
-					tcp_write_pkt(ptr);
+				co_await internal_write_pkt(ptr);
 			}
 		}
 
@@ -422,8 +417,10 @@ namespace avpn {
 			m_identity == Identity::avpn_server)
 			return Proto::avpn_tcp;
 
-		if (params.mode_ == Proto::avpn_tcp ||
-			ipproto == Proto::avpn_mix)
+		if ((params.mode_ == Proto::avpn_tcp ||
+			ipproto == Proto::avpn_mix) &&
+			m_tcp_deque.empty() &&
+			std::rand() % 2 == 0)
 			return Proto::avpn_tcp;
 
 		return Proto::avpn_udp;
