622a7601dc494d8fd8cbb035fb6b285ac38e5c30
jack <jack.wgm@gmail.com>
2022-07-14 08:09:54 +0800

    Add vpn_conntrack module

---
+++ avpn/include/avpn/vpn_conntrack.hpp
@@ -0,0 +1,30 @@
+﻿//
+// Copyright (C) 2022 Jack.
+//
+// Author: jack
+// Email:  jack.wgm at gmail dot com
+//
+
+#include "avpn/avpn.hpp"
+
+
+namespace avpn {
+
+	class vpn_conntrack
+	{
+		// c++11 noncopyable.
+		vpn_conntrack(const vpn_conntrack&) = delete;
+		vpn_conntrack& operator=(const vpn_conntrack&) = delete;
+	public:
+		vpn_conntrack(net::io_context& ioc, std::weak_ptr<avpn_service> service);
+		~vpn_conntrack();
+
+	private:
+		// 用于当前vpn_conntrack业务调度.
+		net::io_context& m_io_context;
+
+		// service 对象引用.
+		std::weak_ptr<avpn_service> m_serivce;
+	};
+
+}
---
+++ avpn/src/main/vpn_conntrack.cpp
@@ -0,0 +1,23 @@
+﻿//
+// Copyright (C) 2022 Jack.
+//
+// Author: jack
+// Email:  jack.wgm at gmail dot com
+//
+
+#include "avpn/vpn_conntrack.hpp"
+
+namespace avpn {
+
+	vpn_conntrack::vpn_conntrack(net::io_context& ioc,
+		std::weak_ptr<avpn_service> service)
+		: m_io_context(ioc)
+		, m_serivce(service)
+	{
+	}
+
+	vpn_conntrack::~vpn_conntrack()
+	{
+	}
+
+}
