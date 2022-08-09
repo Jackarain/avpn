2e08958390fc62158449c66430e9d19f000a7c49
jack <jack.wgm@gmail.com>
2022-08-09 21:51:46 +0800

    Code style for socks server.

--- avpn/include/socks/socks_server.hpp
+++ avpn/include/socks/socks_server.hpp
@@ -43,7 +43,8 @@ namespace socks {
 		socks_session& operator=(const socks_session&) = delete;
 
 	public:
-		socks_session(tcp::socket&& socket, size_t id, std::weak_ptr<socks_server_base> server);
+		socks_session(tcp::socket&& socket,
+			size_t id, std::weak_ptr<socks_server_base> server);
 		~socks_session();
 
 	public:
