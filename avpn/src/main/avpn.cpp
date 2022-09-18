1db64463dc385d64d78d6bc8b62028b4366936ed
jack <jack.wgm@gmail.com>
2022-09-18 14:11:23 +0800

    Fix warnging in avpn.cpp

--- avpn/src/main/avpn.cpp
+++ avpn/src/main/avpn.cpp
@@ -234,7 +234,7 @@ namespace avpn {
 
 		if (std::filesystem::exists(pwd))
 			m_ssl_ctx.set_password_callback(
-				[this, &pwd]([[maybe_unused]] auto... args) {
+				[&pwd]([[maybe_unused]] auto... args) {
 					std::string password;
 					fileop::read(pwd, password);
 					return password;
