9b5714991ce23386ce555b35013f7d02d54a92bc
jack <jack.wgm@gmail.com>
2022-09-21 23:06:49 +0800

    Increase the tun reads to 64

--- avpn/src/main/avpn.cpp
+++ avpn/src/main/avpn.cpp
@@ -957,7 +957,7 @@ namespace avpn {
 
 		// 开始读取tun上的数据包.
 		auto self = shared_from_this();
-		for (int n = 0; n < 16; n++)
+		for (int n = 0; n < 64; n++)
 		{
 			net::co_spawn(m_main_context,
 				[this, self]() mutable -> net::awaitable<void>
