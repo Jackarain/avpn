f48903dcf2cc8cc1506bae6dd4636188efcf80c1
jack <jack.wgm@gmail.com>
2022-09-22 10:33:23 +0800

    Update unittests

--- unittests/network_test/test.cpp
+++ unittests/network_test/test.cpp
@@ -104,44 +104,8 @@ BOOST_AUTO_TEST_CASE(udp_test)
 {
 	net::io_context ioc;
 
-	sender s(ioc);
-	s.run();
+// 	sender s(ioc);
+// 	s.run();
 
 	ioc.run();
 }
-
-#if 0
-BOOST_AUTO_TEST_CASE(fec_cache2_test1)
-{
-	avpn::vpn_packet pkt1;
-	pkt1.size_ = 100;
-
-	avpn::vpn_packet pkt2 = std::move(pkt1);
-
-	BOOST_TEST(pkt1.size_ == 0);
-	BOOST_TEST(pkt2.size_ == 100);
-
-	for (int i = 0; i < 100; i++)
-	{
-		avpn::vpn_packet p;
-		p.size_ = 100;
-	}
-
-	avpn::vpn_packet pkt3;
-	avpn::vpn_packet pkt4;
-}
-
-BOOST_AUTO_TEST_CASE(fec_cache2_test2)
-{
-	avpn::fec_decode_group gop(8, 4);
-	avpn::vpn_packet pkt;
-
-	avpn::vpn_packet_ptr ptr =
-		std::make_shared<avpn::vpn_packet>(std::move(pkt));
-
-	gop.update(1, 0, ptr);
-
-	BOOST_TEST(ptr->size() == avpn::avpn_packet_size);
-	BOOST_TEST(gop.total_ == avpn::avpn_packet_size);
-}
-#endif
