d9aa3505b9e19cd9314cb9f86a7c9163d3a260fd
Jackarain <jack.wgm@gmail.com>
2026-08-21 02:04:35 +0800

    Rename CSA_PATH to AVPN_PATH in android build script

--- build.android.sh
+++ build.android.sh
@@ -3,7 +3,7 @@
 #
 # 使用方法:
 #
-# ./build.android.sh <path-to-csa> <path-to-ndk> <host-tag>
+# ./build.android.sh <path-to-avpn> <path-to-ndk> <host-tag>
 # 示例:
 # ./build.android.sh ~/Documents/avpn /Users/jack/Library/Android/sdk/ndk/26.1.10909125
 # ./build.android.sh ~/Documents/avpn /Users/jack/Library/Android/sdk/ndk/26.1.10909125 darwin-x86_64
@@ -13,7 +13,7 @@
 
 ARCHITECTURES=("arm64-v8a" "armeabi-v7a" "x86" "x86_64")
 
-CSA_PATH=$1
+AVPN_PATH=$1
 NDK_PATH=$2
 HOST_TAG=${3:-windows-x86_64}
 BUILD_TYPE="Release"
@@ -28,13 +28,13 @@ elif [ "$kernel" = "MINGW64_NT-10.0" ]; then
     HOST_TAG=windows-x86_64
 fi
 
-echo "CSA_PATH: ${CSA_PATH}"
+echo "AVPN_PATH: ${AVPN_PATH}"
 echo "NDK_PATH: ${NDK_PATH}"
 echo "HOST_TAG: ${HOST_TAG}"
 
 for ARCH in "${ARCHITECTURES[@]}"
 do
-    cmake -S ${CSA_PATH} -B android/$ARCH -DCMAKE_TOOLCHAIN_FILE=${NDK_PATH}/build/cmake/android.toolchain.cmake -DANDROID_ABI=${ARCH} -DANDROID_PLATFORM=android-19 -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -G Ninja
+    cmake -S ${AVPN_PATH} -B android/$ARCH -DCMAKE_TOOLCHAIN_FILE=${NDK_PATH}/build/cmake/android.toolchain.cmake -DANDROID_ABI=${ARCH} -DANDROID_PLATFORM=android-19 -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -G Ninja
     cmake --build android/$ARCH
     mkdir -p release/$ARCH
     cp android/$ARCH/bin/* release/$ARCH/
