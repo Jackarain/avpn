06312760614b023c4cffab6ff8e52581ea124b80
jackarain <jack.wgm@gmail.com>
2026-09-11 02:17:33 +0800

    Update flutter project for Flutter 3.47 toolchain upgrade

--- avpn/android/xavpn/android/app/build.gradle.kts
+++ avpn/android/xavpn/android/app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "com.jackarain.xavpn"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        // VpnService.protect(int) 需要 API 22+, 这里直接使用 23.
-        minSdk = 23
+        // VpnService.protect(int) 需要 API 22+, 跟随 Flutter 默认值 (当前 24).
+        minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
